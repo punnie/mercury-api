63b545295012cefc0e28c1f0f6a68a3d97530edc
Pedro Coelho <punnie@ethereal.io>
2021-01-31 14:34:30 +0000

    Send modern user agent

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ app.get('/', function (req, res) {
 
   if(url) {
     console.log(`Fetching '${url}'`);
-    mercury.parse(req.query.url).then(result => res.send(result));
+
+    mercury.parse(req.query.url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'
+      }
+    }).then(result => res.send(result));
   } else {
     console.log(`URL can't be empty!`);
   }
