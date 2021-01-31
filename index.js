45b426853df7f6383a5dcbcd4bd2cd0fe61dfbab
Pedro Coelho <punnie@ethereal.io>
2021-01-31 14:36:17 +0000

    Change default listening message

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Mercury API listening on port ${port}!`);
 });
